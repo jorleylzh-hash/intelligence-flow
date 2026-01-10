534acd997f6804f02375f8c21c23d92e87270b74
jorleylzh-hash <jorleylzh@gmail.com>
2026-01-10 19:32:45 -0300

    Update auth_engine.py

--- modules/auth_engine.py
+++ modules/auth_engine.py
@@ -2,24 +2,26 @@ import streamlit_authenticator as stauth
 import streamlit as st
 
 def get_authenticator():
-    # Configuração explícita e correta
+    # Hash gerado para a senha "123"
+    # Senha: 123
+    hashed_pass = "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga311W" 
+    
     users_config = {
         "usernames": {
             "admin": {
                 "name": "Admin Trader",
                 "email": "admin@intelligenceflow.com",
-                "password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga311W" # Senha: 123
+                "password": hashed_pass
             }
         },
         "cookie": {
             "expiry_days": 30,
-            "key": "random_signature_key_if_2026",
+            "key": "random_signature_key_if_secure",
             "name": "auth_cookie_if"
         },
         "preauthorized": {"emails": []}
     }
 
-    # Instanciação direta
     authenticator = stauth.Authenticate(
         users_config,
         users_config['cookie']['name'],
