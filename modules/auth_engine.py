af5b0f800a2fcd166b0958b6929c73ea7ee847a1
jorleylzh-hash <jorleylzh@gmail.com>
2026-01-10 21:17:07 -0300

    Update auth_engine.py

--- modules/auth_engine.py
+++ modules/auth_engine.py
@@ -2,24 +2,23 @@ import streamlit_authenticator as stauth
 import streamlit as st
 
 def get_authenticator():
-    # --- CORREÇÃO DEFINITIVA DO HASH ---
-    # Geramos o hash da senha "123" em tempo real para garantir compatibilidade
-    # com a versão instalada no servidor.
-    passwords_to_hash = ['123']
-    hashed_passwords = stauth.Hasher(passwords_to_hash).generate()
+    # --- SENHA '123' JÁ CRIPTOGRAFADA ---
+    # Isso elimina o erro de "Hasher.__init__" pois não calculamos nada na hora.
+    # Este código abaixo equivale a "123"
+    hashed_pass = "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga311W"
     
     users_config = {
         "usernames": {
             "admin": {
                 "name": "Head Trader",
                 "email": "admin@intelligenceflow.com",
-                "password": hashed_passwords[0] # Usa o hash gerado agora
+                "password": hashed_pass
             }
         },
         "cookie": {
             "expiry_days": 1,
-            "key": "random_signature_key_final_v5",
-            "name": "auth_cookie_v5"
+            "key": "random_signature_key_final_v6",
+            "name": "auth_cookie_v6"
         },
         "preauthorized": {"emails": []}
     }
